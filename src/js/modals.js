a7b679a27326e14027821254f50052a52e69e5f0
Bernardo Sanchez <bernardoluisanchez25@gmail.com>
2022-09-02 18:20:25 +0200

    change modals

--- src/js/modals.js
+++ src/js/modals.js
@@ -17,7 +17,7 @@ allBtnCards.forEach(btn => {
             cuerpo.style.overflow = "hidden";
             modal.innerHTML =`
             <h3>Marketing</h3>
-            <p>blogs, newsletters, photography and video <span style="color: #FF7223">content creation</span> according to the platforms we are working with.</p>
+            <p><span style="color: #FF7223">I create the strategic plan</span> for social media platforms, such as Instagram, TikTok, and Facebook, including Facebook ads,Instagram ads, and Google ads. Google analytics, all kinds of digital campaigns and email marketing</p>
             <h6 style="color: #FF7223; cursor: pointer">Close</h6>`;
         } else if (card.id == "SEO") {
             modal.classList.add("show-modal");
@@ -41,7 +41,7 @@ allBtnCards.forEach(btn => {
             cuerpo.style.overflow = "hidden";
             modal.innerHTML =`
             <h3>Content Creation</h3>
-            <p><span style="color: #FF7223">I create the strategic plan</span> for social media platforms, such as Instagram, TikTok, and Facebook, including Facebook ads,Instagram ads, and Google ads. Google analytics, all kinds of digital campaigns and email marketing</p>
+            <p>blogs, newsletters, photography and video <span style="color: #FF7223">content creation</span> according to the platforms we are working with.</p>
             <h6 style="color: #FF7223; cursor: pointer">Close</h6>`;
         }
     });
